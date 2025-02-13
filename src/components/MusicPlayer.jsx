d2e7fc322db69cfeb6872aa260d250c9ec0724c7
BerserKess <kesleyrainery.21@gmail.com>
2025-02-13 15:08:22 -0300

    feat: Criada pagina Song e componente MusicPlayer

---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaPlayCircle } from 'react-icons/fa'
+import { FaBackwardStep, FaForwardStep } from 'react-icons/fa6'
+import { Link } from 'react-router-dom'
+
+
+const MusicPlayer = () => {
+    return (
+        <div className='flex flex-col gap-1.5 items-center justify-self-stretch'>
+            <div className='flex items-center gap-5 text-2xl'>
+                <Link to={"/song/0"}>
+                    <FaBackwardStep className="text-green-50 cursor-pointer  transition-colors duration-200 ease-in hover:scale-105 hover:text-[#20df63]" />
+                </Link>
+                <FaPlayCircle className="text-green-50 cursor-pointer  transition-colors duration-200 ease-in hover:scale-105 hover:text-[#20df63] text-4xl" />
+                <Link to={"/song/2"}>
+                    <FaForwardStep className="text-green-50 cursor-pointer  transition-colors duration-200 ease-in hover:scale-105 hover:text-[#20df63]" />
+                </Link>
+            </div>
+            <div className='flex items-center gap-2.5 w-full max-w-[600px]'>
+                <p>00:00</p>
+                <div className='h-1 bg-gray-500 w-full rounded-2xl'>
+                    <div className='h-full bg-green-500 transition-[width] duration-[ease_ease]' style={{ width: "0%" }}></div>
+                </div>
+                <p>02:30</p>
+            </div>
+        </div>
+    )
+}
+
+export default MusicPlayer
--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import MusicPlayer from '../components/MusicPlayer'
+import { Link } from 'react-router-dom'
+
 
 const Song = () => {
     return (
-        <div>Song</div>
+        <section className='grid grid-rows-1 flex-1 '>
+            <div className='flex flex-col bg-body gap-10 my-0 mx-4 p-6 rounded-2xl rounded-b-none '>
+                <div className=' flex items-center justify-center flex-1 '>
+                    <img className='shadow-[0_0_20px_10px_#020b0580]' src="https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d" alt="Imagem da música X" />
+                </div>
+            </div>
+
+            <div className='bg-[#020b05] py-4 px-6 grid grid-cols-[1fr_3fr_1fr] place-items-center flex-col gap-6 text-[#d2f9e0]'>
+                <Link to={"/artist/1"} className='justify-self-start rounded-xl overflow-hidden'>
+                    <img className='w-20 h-20' src="https://i.scdn.co/image/ab6761610000e5ebe78f7b1edae902872d399cfb" alt="Imagem do artista Y" />
+
+                </Link>
+
+                <MusicPlayer />
+
+                <div>
+                    <p className='text-xl font-bold'>Cantada Boba - Ao Vivo</p>
+                    <p>Wind Rose</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
